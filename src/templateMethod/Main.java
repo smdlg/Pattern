3dcab54f2337d90c252ab63806b1f36dcc6eeda2
smdlg <smdlg1004@gmail.com>
2018-11-26 12:19:33 +0900

    template Method End

--- src/templateMethod/Main.java
+++ src/templateMethod/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		AbstractDisplay d1 = new CharDisplay('H');
+		
+		AbstractDisplay d2 = new StringDisplay("Hello, world");
+		
 		d1.display();
+		d2.display();
 	}
 
 }
